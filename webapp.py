7941b1d97c1cdd52822c74c9dedc99f29dad98a4
Kylan O'Connor <kylan02@gmail.com>
2019-11-18 09:16:50 -0800

    commit

--- webapp.py
+++ webapp.py
@@ -1,1 +1,14 @@
+from flask import Flask, request, Markup, render_template, flash, Markup
+import os
+import json
 
+app = Flask(__name__)
+
+@app.route("/")
+def render_main():
+	with open('wind_turbines.json') as turbine_data:
+		counties = json.load(turbine_data)
+	return render_template('home.html')
+
+if __name__=="__main__":
+    app.run(debug=False)
