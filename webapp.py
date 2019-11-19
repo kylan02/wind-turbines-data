8417f5df612b576823046303c7c1f4861d3a226d
Kylan O'Connor <kylan02@gmail.com>
2019-11-19 09:46:16 -0800

    commit

--- webapp.py
+++ webapp.py
@@ -9,6 +9,22 @@ def render_main():
 	with open('wind_turbines.json') as turbine_data:
 		counties = json.load(turbine_data)
 	return render_template('home.html')
-
+	
+@app.route("/ByState")
+def render_page1():
+	return render_template('state.html')
+	
+@app.route("/ByRotorSize")
+def render_page2():
+	return render_template('rotor.html')
+	
+@app.route("/ByYear")
+def render_page3():
+	return render_template('year.html')
+	
+@app.route("/ByHeight")
+def render_page4():
+	return render_template('height.html')
+	
 if __name__=="__main__":
     app.run(debug=False)
